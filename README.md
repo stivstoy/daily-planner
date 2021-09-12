19938b2e62a46a5c217334dd584eaad61aaf565e
stivstoy <stevehutson@gmail.com>
2021-09-12 15:34:19 -0500

    fully functional

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Work Day Scheduler Starter Code
+# Challenge 5 UT Coding Bootcamp
+
+## Purpose
+Create a daily planner that displays the current day and time that you can enter tasks and save.
+## Built With
+* HTML
+* CSS
+* JavaScript
+* 3rd Party API (Moment.js)
+
+## Website
+https://stivstoy.github.io/code-quiz/
+
+![quiz](https://user-images.githubusercontent.com/87504797/132140507-57f2d020-957a-4915-9447-d408cbefd1f1.jpg)
+
+## Contribution
+Made with ❤️ by Steve Hutson
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,131 @@
 var day = moment().format('MMMM Do YYYY');
 var hour = moment().format('h');
-var description =""
-const hours =[9, 10, 11, 12, 1, 2, 3, 4, 5]
-
+var description =  '';
+var hourDisplay = "";
+const hours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+var schedule = [];
+var descriptionValue ='';
+var textAreaValue;
+var descRows;
 document.getElementById("currentDay").innerHTML = day;
 
+//checking for local storage
+if (JSON.parse(window.localStorage.getItem('scheduleItems'))) {
+    schedule = JSON.parse(window.localStorage.getItem('scheduleItems'));    
+}
+else {
+   schedule = [];
+}
+
+//display schedule layout
 timeDisplay();
 
+//display scheduled items stored
+displayItems();
+
+// function part of the textarea Event Listener to monitor additions and changes to textarea values to store     
+function updateValue(e) {
+       id = e.target.id;
+       descriptionValue = e.target.value;                    
+ }
+
+// onClick function of the save button to retrieve the description entered and id (selected time) to store
+ function descValue (){
+         storeDesc(descriptionValue,id);
+ }
+  
+ //function to build and display schedule layout
 function timeDisplay() {
-    var text = '';
-    for (var i = 0; i < hours.length; i++) {
-       var hourDisplay = text =hours[i];
-       let div = document.createElement('div');
-         div.id = 'container';
-          div.className = 'container';
-          document.body.appendChild(div);        
-          div.innerHTML = "<div class ='row' id = 'row'><div id = 'hour' div class='hour'><script>document.log hourdisplay</script> </div><div id ='description' div class='description'> this is description </div> <div class='saveBtn' div id='saveBtn'> save</div>";
+        for (var i = 0; i < hours.length; i++) {
+        hourDisplay = hours[i];  
+       let container = document.createElement('div');           
+        container.className = 'container';        
+        document.body.appendChild(container);
+        var row = document.createElement('div');
+        row.className ='row';        
+        container.appendChild(row);
+        let hour = document.createElement('div');
+        hour.className ='hour';
+        row.appendChild(hour);
+        let description= document.createElement('div');
+        description.className ='description';
+         var textArea = document.createElement("TEXTAREA");
+        textArea.setAttribute('name', 'post'+hourDisplay);
+        textArea.id= hourDisplay;
+        textArea.value ='';               
+        description.appendChild(textArea);
+        row.appendChild(description);
+        let saveBtn= document.createElement('div');
+        saveBtn.className ='saveBtn';
+        var btn = document.createElement("BUTTON");
+        btn.id= 'btn'+hourDisplay;
+        btn.setAttribute("onclick", "descValue()");
+         btn.innerHTML = "Save";
+        btn.setAttribute("type", "submit");
+        saveBtn.appendChild(btn);
+        row.appendChild(saveBtn);
 
-            }
-        }   
+        //convert military time values from the hours array to standard time to display on the schedule layout
+        hour.innerHTML = moment(hourDisplay, 'HH').format('h A');
+
+        //identify textarea element to add an event listener
+        textAreaValue = document.getElementById(textArea.id);
+        
+        //set event listener to monitor textarea value changes
+        textAreaValue.addEventListener('change', updateValue);
+     
+        // call function to set time block color code  
+        backgroundDisplay();
+        
+  //function to set color-coded time block to indicate whether time displayed is in the past, present, or future      
+function backgroundDisplay() {       
+        hour = moment().format('HH');
+         if(hourDisplay > hour) {
+         descRows = document.getElementsByClassName("description");
+        descRows[i].className += " future";
+         }
     
+        if(hourDisplay == hour) {
+        descRows= document.getElementsByClassName("description");
+        descRows[i].className += " present";
+        }
+    
+        if ( hourDisplay < hour) {
+        descRows = document.getElementsByClassName("description");
+        descRows[i].className += " past";
+        }   
+         
+       }
+    }
+    
+    }
 
+// function to store textarea scheduled items
+function storeDesc(description, hourDisplay) {
+    const hoursDataObj = {
+        desc: description,
+        time: hourDisplay
+    }
+ 
+     schedule.push(hoursDataObj);
+     console.log(schedule);
+     window.localStorage.setItem('scheduleItems', JSON.stringify(schedule));
+     window.localStorage.getItem('scheduleItems');
+      JSON.parse(window.localStorage.getItem('scheduleItems'));
+     displayItems();
+   
+}
 
+// function to display stored scheduled items
+function displayItems() {
+   
+    for (var i = 0; i < schedule.length; i++) {
+      var textDescHour = document.getElementById(schedule[i].time);
+      var textDescHourId = textDescHour.id;
+      if (schedule[i].time === textDescHourId){
+       document.getElementById(schedule[i].time).value = schedule[i].desc;
+        document.getElementById(schedule[i].time).innerHTML =  schedule[i].desc;
+        }
+
+        }
+}
